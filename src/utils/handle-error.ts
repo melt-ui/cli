b97e12a23ce0d4a7e415937a985f2eca6020e264
CokaKoala <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-20 12:24:48 -0500

    chore: Adds an error message if `preprocess` is missing in the svelte config (#11)

--- src/utils/handle-error.ts
+++ src/utils/handle-error.ts
@@ -1,16 +1,19 @@
 import { logger } from './logger.js';
 
-export function handleError(error: unknown) {
+export function handleError(error: unknown): never {
+	const PREFIX = 'ERROR:';
+	logger.info('\n');
+
 	if (typeof error === 'string') {
-		logger.error(error);
-		return (process.exitCode = 1);
+		logger.error(`${PREFIX} ${error}`);
+		process.exit(1);
 	}
 
 	if (error instanceof Error) {
-		logger.error(error.message);
-		return (process.exitCode = 1);
+		logger.error(`${PREFIX} ${error.message}`);
+		process.exit(1);
 	}
 
-	logger.error('Something went wrong. Please try again.');
-	return (process.exitCode = 1);
+	logger.error(`${PREFIX} Something went wrong. Please try again.`);
+	process.exit(1);
 }
